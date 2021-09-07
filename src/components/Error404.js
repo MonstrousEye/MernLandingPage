42a76700140a205910f31b74a6c9863b0d489aa0
manjunath <manjunathrs7826@gmail.com>
2021-09-07 09:05:55 +0530

    f

---
+++ src/components/Error404.js
@@ -0,0 +1,11 @@
+function Error404(){
+    return(
+        <div>
+            <h1> Page not found </h1>
+            <p>This Page can not be found</p>
+            <a href='/'>Go Home</a>
+        </div>
+    )
+}
+
+export default Error404;
